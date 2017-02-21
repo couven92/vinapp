da0c00706020d4ff6a648c58b167ae833cebf018
Njaal Gjerde <njaal.gjerde@gmail.com>
2017-02-21 20:40:50 +0100

    Adding drawing of ticket functionality

--- public/index.js
+++ public/index.js
@@ -1,17 +1,48 @@
+var tickets = [];
+for (i = 0; i < 100; i++) {
+    tickets[i] = { number: i+1, name: "njaal", paid: true, color: "" }
+}
 var app = new Vue({
     el: '#app',
     data: {
-        tickets: null
+        tickets: tickets
     },
     methods: {
         lottery: function (event) {
-          
+            var arr = []
+            while (arr.length < 99) {
+                var randomnumber = Math.ceil(Math.random() * (99))
+                if (arr.indexOf(randomnumber) > -1) continue;
+                arr[arr.length] = randomnumber;
+            }
+
+            function highlightStuff(element, index, elements) {
+                return function () {
+                    console.log(element + " - "+index);
+                    this.tickets[element].color = "highlighted";
+                    if (index == elements.length - 1) {
+                        this.tickets[element].winner = true;
+                        this.tickets[element].color = "winner";
+                    }
+                    if (index + 1 < elements.length) {
+                        setTimeout(highlightStuff(elements[index + 1], index + 1, elements), 200);
+                    } else {
+                        elements.forEach(function (element, index, elements) {
+                            this.tickets[element].highlighted = false;
+                            if (!index == elements.length) {
+                                this.tickets[element].color = "";
+                            }
+                        })
+                    }
+                }
+            }
+
+            setTimeout(highlightStuff(arr[0], 0, arr), 200);
+
+
         }
     }
-})
+});
 
-var tickets = [];
-for (i = 0; i < 100; i++) {
-    tickets[i] = { number: i + 1, name: "njaal", paid: true }
-}
-app.tickets = tickets;
+
+
