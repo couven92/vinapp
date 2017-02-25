052cc542bd1adbf84c78e2bbd5582543927f9a8f
ivanmilano <ivan@mivaco.com>
2017-02-25 12:38:28 +0100

    Added swagger for documentation

--- api/Vinapp/src/Vinapp.Api/Startup.cs
+++ api/Vinapp/src/Vinapp.Api/Startup.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 using Vinapp.Api.Services;
 using Vinapp.Data;
 using Vinapp.Data.Dal;
@@ -24,7 +27,7 @@ namespace Vinapp.Api
             _config = builder.Build();
         }
 
-        private IConfigurationRoot _config;
+        readonly IConfigurationRoot _config;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,12 +35,28 @@ namespace Vinapp.Api
             services.AddSingleton(_config);
             services.AddDbContext<VinappContext>(ServiceLifetime.Scoped).AddIdentity<User, IdentityRole>();
 
-
             // Add framework services.
             services.AddMvc();
 
             services.AddTransient<ILotteryTicketService, LotteryTicketService>();
             services.AddTransient<ILotteryTicketRepository, LotteryTicketRepository>();
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new Info
+                {
+                    Title = "Vinapp.API",
+                    Version = "v1",
+                    Description = "Vinapp.API provides backend for Vinapp",
+                    Contact = new Contact { Name = "Ivan & Njaal @Ciber.no"},
+                    License = new License { Name = "MIT/X11", Url = "https://opensource.org/licenses/MIT" }
+                });
+
+                //Set the comments path for the swagger json and ui.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "Vinapp.Api.xml");
+                config.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +65,13 @@ namespace Vinapp.Api
             loggerFactory.AddConsole(_config.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            app.UseStaticFiles();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi(config =>
+            {
+                config.SwaggerEndpoint("/swagger/v1/swagger.json", "Vinapp API v1");
+            });
         }
     }
 }
