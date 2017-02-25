2f603f592e3499b7ffe4fea4604b165ce941f5ca
ivanmilano <ivan@mivaco.com>
2017-02-25 12:38:58 +0100

    Updated controller methods to support documentation

--- api/Vinapp/src/Vinapp.Api/Controllers/SettingsController.cs
+++ api/Vinapp/src/Vinapp.Api/Controllers/SettingsController.cs
@@ -2,10 +2,19 @@
 
 namespace Vinapp.Api.Controllers
 {
+    /// <summary>
+    /// Endpoint for providing configuration values
+    /// </summary>
     [Route("api/[controller]")]
     public class SettingsController : Controller
     {
+        /// <summary>
+        /// Provides possible tickets to use at any moment
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Object</response>
         [HttpGet("ticketsToUse")]
+        [ProducesResponseType(typeof(object), 200)]
         public IActionResult TicketsToUse()
         {
             return Ok(new {Amount = 100});
