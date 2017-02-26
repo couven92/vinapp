9f84182745a0db285549dba9078b6d8f5060158f
ivanmilano <ivan@mivaco.com>
2017-02-27 00:35:43 +0100

    Introduced Identity and Tokens

--- src/Vinapp.Api/Startup.cs
+++ src/Vinapp.Api/Startup.cs
@@ -1,17 +1,23 @@
 ﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.Swagger;
 using Vinapp.Api.Services;
 using Vinapp.Data;
+using Vinapp.Data.Context;
 using Vinapp.Data.Dal;
 using Vinapp.Data.Models;
+#pragma warning disable 1591
 
 namespace Vinapp.Api
 {
@@ -33,10 +39,47 @@ namespace Vinapp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(_config);
-            services.AddDbContext<VinappContext>(ServiceLifetime.Scoped).AddIdentity<User, IdentityRole>();
+            services.AddDbContext<VinappContext>(ServiceLifetime.Scoped);
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<VinappContext>();
+            services.AddTransient<VinappIdentityInitializer>();
+
+            services.Configure<IdentityOptions>(cfg =>
+            {
+                cfg.Cookies.ApplicationCookie.Events =
+                    new CookieAuthenticationEvents
+                    {
+                        OnRedirectToLogin = (ctx) =>
+                        {
+                            if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+                            {
+                                ctx.Response.StatusCode = 401;
+                            }
+
+                            return Task.CompletedTask;
+                        },
+                        OnRedirectToAccessDenied = ctx =>
+                        {
+                            if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+                            {
+                                ctx.Response.StatusCode = 403;
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+            });
+            services.AddAuthorization(cfg =>
+            {
+                cfg.AddPolicy("SuperUsers", p => p.RequireClaim("SuperUser", "True"));
+                cfg.AddPolicy("AdminUsers", p => p.RequireClaim("AdminUser", "True"));
+                cfg.AddPolicy("RegularUsers", p => p.RequireClaim("RegularUser", "True"));
+            });
 
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
 
             services.AddTransient<ILotteryTicketService, LotteryTicketService>();
             services.AddTransient<ILotteryTicketRepository, LotteryTicketRepository>();
@@ -66,7 +109,25 @@ namespace Vinapp.Api
             loggerFactory.AddDebug();
 
             app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions()
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = _config["Tokens:Issuer"],
+                    ValidAudience = _config["Tokens:Audience"],
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"])),
+                    ValidateLifetime = true
+                }
+            });
+
             app.UseMvc();
+
             app.UseSwagger();
             app.UseSwaggerUi(config =>
             {
