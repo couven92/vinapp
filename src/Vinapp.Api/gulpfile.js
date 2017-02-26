93e25b70500873e4d683d53cdef2c4841218c104
Njaal Gjerde <njaal.gjerde@gmail.com>
2017-02-26 17:11:42 +0100

    Deleting some folders, moving static web part inside the wwwroot folder in the api project

---
+++ src/Vinapp.Api/gulpfile.js
@@ -0,0 +1,8 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function () {
+    gulp.src('src/Vinapp.Api/Sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/Vinapp.Api/wwwroot/css/'));
+});
