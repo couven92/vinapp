6903a16c52cdf449b81a7930effbd2c34190b35e
Njaal Gjerde <njaal.gjerde@gmail.com>
2017-02-26 21:38:01 +0100

    Getting number of tickets from the api. Adding some styling and some functionality to buy tickets.

--- src/Vinapp.Api/wwwroot/index.js
+++ src/Vinapp.Api/wwwroot/index.js
@@ -1,47 +1,53 @@
-var tickets = [];
-for (i = 0; i < 100; i++) {
-    tickets[i] = { number: i+1, name: "njaal", paid: true, color: "" }
-}
-var app = new Vue({
-    el: '#app',
-    data: {
-        tickets: tickets
-    },
-    methods: {
-        lottery: function (event) {
-            var arr = []
-            while (arr.length < 100) {
-                var randomnumber = Math.ceil(Math.random() * (100)-1)
-                if (arr.indexOf(randomnumber) > -1) continue;
-                arr[arr.length] = randomnumber;
-            }
 
-            function highlightStuff(element, index, elements) {
-                return function () {
-                    console.log(element + " - "+index);
-                    this.tickets[element].color = "highlighted";
-                    if (index == elements.length - 1) {
-                        this.tickets[element].winner = true;
-                        this.tickets[element].color = "winner";
-                    }
-                    if (index + 1 < elements.length) {
-                        setTimeout(highlightStuff(elements[index + 1], index + 1, elements), 200);
-                    } else {
-                        elements.forEach(function (element, index, elements) {
-                            this.tickets[element].highlighted = false;
-                            if (!index == elements.length) {
-                                this.tickets[element].color = "";
-                            }
-                        })
+$.getJSON('/api/Settings/ticketsToUse', function (data) {
+    var username = "user";
+    var tickets = [];
+    for (i = 0; i < data.amount; i++) {
+        tickets[i] = { number: i + 1, name: "", bought: false, paid: false, color: "" };
+    }
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            tickets: tickets
+        },
+        methods: {
+            lottery: function (event) {
+                var arr = [];
+                while (arr.length < data.amount) {
+                    var randomnumber = Math.ceil(Math.random() * (data.amount) - 1);
+                    if (arr.indexOf(randomnumber) > -1) continue;
+                    arr[arr.length] = randomnumber;
+                }
+
+                function highlightStuff(element, index, elements, vue) {
+                    return function () {
+                        vue.tickets[element].color = "highlighted";
+                        if (index == elements.length - 1) {
+                            vue.tickets[element].winner = true;
+                            vue.tickets[element].color = "winner";
+                        }
+                        if (index + 1 < elements.length) {
+                            setTimeout(highlightStuff(elements[index + 1], index + 1, elements, vue), 200);
+                        } else {
+                            elements.forEach(function(element, index, elements) {
+                                vue.tickets[element].highlighted = false;
+                                if (!index == elements.length) {
+                                    vue.tickets[element].color = "";
+                                }
+                            });
+                        }
                     }
                 }
+                setTimeout(highlightStuff(arr[0], 0, arr, this), 200);
+            },
+            buy: function(ticketnumber) {
+                this.tickets[ticketnumber - 1].bought = true;
+                this.tickets[ticketnumber - 1].name = username;
             }
-
-            setTimeout(highlightStuff(arr[0], 0, arr), 200);
-
-
         }
-    }
+    });
+
 });
 
 
