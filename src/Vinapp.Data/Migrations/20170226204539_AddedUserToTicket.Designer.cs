f9a27601a689f4aa8a8c50c0d8b91006af3fad89
ivanmilano <ivan@mivaco.com>
2017-02-27 00:31:40 +0100

    Added migrations for supporting user on LotteryTicket

--- src/Vinapp.Data/Migrations/20170226204539_AddedUserToTicket.Designer.cs
+++ src/Vinapp.Data/Migrations/20170226204539_AddedUserToTicket.Designer.cs
@@ -8,9 +8,10 @@ using Vinapp.Data;
 namespace Vinapp.Data.Migrations
 {
     [DbContext(typeof(VinappContext))]
-    partial class VinappContextModelSnapshot : ModelSnapshot
+    [Migration("20170226204539_AddedUserToTicket")]
+    partial class AddedUserToTicket
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
---
+++ src/Vinapp.Data/Migrations/20170226204539_AddedUserToTicket.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vinapp.Data.Migrations
+{
+    public partial class AddedUserToTicket : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "LotteryTickets",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LotteryTickets_UserId",
+                table: "LotteryTickets",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LotteryTickets_AspNetUsers_UserId",
+                table: "LotteryTickets",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LotteryTickets_AspNetUsers_UserId",
+                table: "LotteryTickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LotteryTickets_UserId",
+                table: "LotteryTickets");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "LotteryTickets");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
