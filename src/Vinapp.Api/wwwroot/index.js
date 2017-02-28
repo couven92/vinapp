720488edacc1832e461abec155aeea71a9cdc214
Njaal Gjerde <njaal.gjerde@gmail.com>
2017-02-28 15:34:49 +0100

    Check token...

--- src/Vinapp.Api/wwwroot/index.js
+++ src/Vinapp.Api/wwwroot/index.js
@@ -1,53 +1,64 @@
 
-$.getJSON('/api/Settings/ticketsToUse', function (data) {
-    var username = "user";
-    var tickets = [];
-    for (i = 0; i < data.amount; i++) {
-        tickets[i] = { number: i + 1, name: "", bought: false, paid: false, color: "" };
-    }
-
-    var app = new Vue({
-        el: '#app',
-        data: {
-            tickets: tickets
-        },
-        methods: {
-            lottery: function (event) {
-                var arr = [];
-                while (arr.length < data.amount) {
-                    var randomnumber = Math.ceil(Math.random() * (data.amount) - 1);
-                    if (arr.indexOf(randomnumber) > -1) continue;
-                    arr[arr.length] = randomnumber;
+$.ajax({
+    url: "/api/auth/token",
+    data: { username: "Njaal", password: "P@ssw0rd!" },
+    type: "GET",
+    success: function (data) {
+        $.ajax({
+            url: "/api/Settings/ticketsToUse",
+            type: "GET",
+            beforeSend: function (xhr) { xhr.setRequestHeader('authorization', 'bearer ' + data.token); },
+            success: function () {
+                var username = "user";
+                var tickets = [];
+                for (i = 0; i < data.amount; i++) {
+                    tickets[i] = { number: i + 1, name: "", bought: false, paid: false, color: "" };
                 }
 
-                function highlightStuff(element, index, elements, vue) {
-                    return function () {
-                        vue.tickets[element].color = "highlighted";
-                        if (index == elements.length - 1) {
-                            vue.tickets[element].winner = true;
-                            vue.tickets[element].color = "winner";
-                        }
-                        if (index + 1 < elements.length) {
-                            setTimeout(highlightStuff(elements[index + 1], index + 1, elements, vue), 200);
-                        } else {
-                            elements.forEach(function(element, index, elements) {
-                                vue.tickets[element].highlighted = false;
-                                if (!index == elements.length) {
-                                    vue.tickets[element].color = "";
+                var app = new Vue({
+                    el: '#app',
+                    data: {
+                        tickets: tickets
+                    },
+                    methods: {
+                        lottery: function (event) {
+                            var arr = [];
+                            while (arr.length < data.amount) {
+                                var randomnumber = Math.ceil(Math.random() * (data.amount) - 1);
+                                if (arr.indexOf(randomnumber) > -1) continue;
+                                arr[arr.length] = randomnumber;
+                            }
+
+                            function highlightStuff(element, index, elements, vue) {
+                                return function () {
+                                    vue.tickets[element].color = "highlighted";
+                                    if (index == elements.length - 1) {
+                                        vue.tickets[element].winner = true;
+                                        vue.tickets[element].color = "winner";
+                                    }
+                                    if (index + 1 < elements.length) {
+                                        setTimeout(highlightStuff(elements[index + 1], index + 1, elements, vue), 200);
+                                    } else {
+                                        elements.forEach(function (element, index, elements) {
+                                            vue.tickets[element].highlighted = false;
+                                            if (!index == elements.length) {
+                                                vue.tickets[element].color = "";
+                                            }
+                                        });
+                                    }
                                 }
-                            });
+                            }
+                            setTimeout(highlightStuff(arr[0], 0, arr, this), 200);
+                        },
+                        buy: function (ticketnumber) {
+                            this.tickets[ticketnumber - 1].bought = true;
+                            this.tickets[ticketnumber - 1].name = username;
                         }
                     }
-                }
-                setTimeout(highlightStuff(arr[0], 0, arr, this), 200);
-            },
-            buy: function(ticketnumber) {
-                this.tickets[ticketnumber - 1].bought = true;
-                this.tickets[ticketnumber - 1].name = username;
+                });
             }
-        }
-    });
-
+        });
+    }
 });
 
 
