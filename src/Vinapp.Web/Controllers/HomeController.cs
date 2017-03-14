c32aa2e265a4d1c51a24f28a84fd8a0bfd3157ab
Njaal Gjerde <njaal.gjerde@gmail.com>
2017-03-14 23:54:42 +0100

    Refactoring

--- src/Vinapp.Web/Controllers/HomeController.cs
+++ src/Vinapp.Web/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.DotNet.Cli.Utils;
-using Newtonsoft.Json;
-using System.Runtime.Serialization;
+using static Newtonsoft.Json.JsonConvert;
 
 namespace Vinapp.Web.Controllers
 {
@@ -16,31 +13,18 @@ namespace Vinapp.Web.Controllers
     {
         public async Task<ViewResult> Index()
         {
-            var token = "";
             if (HttpContext.User == null || !HttpContext.User.Identity.IsAuthenticated)
-                HttpContext.Authentication.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/" });
-            //if (HttpContext.User != null && HttpContext.User.Identity.IsAuthenticated)
-            //{
-                
+                await HttpContext.Authentication.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/" });
 
-            //}
-            Auth something = await GetTokenAsync("Njaal", "P@ssw0rd!");
-            token = something.Token;
-            return View();
+            return View("Index");
         }
 
-        public IActionResult About()
+        public async Task<JsonResult> GetToken()
         {
-            ViewData["Message"] = "Your application description page.";
+            //Auth auth = await GetTokenAsync(HttpContext.User.Identity.Name)
+            Auth auth = await GetTokenAsync("Njaal", "P@ssw0rd!");
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            return Json(auth);
         }
 
         public IActionResult Error()
@@ -48,11 +32,10 @@ namespace Vinapp.Web.Controllers
             return View();
         }
 
-        static async Task<Auth> GetTokenAsync(string username, string password)
+        private static async Task<Auth> GetTokenAsync(string username, string password = "")
         {
-            var path = "api/auth/token";
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:9888/");
+            const string path = "api/auth/token";
+            var client = new HttpClient {BaseAddress = new Uri("http://localhost:9888/")};
 
             var login = new Login
             {
@@ -60,20 +43,16 @@ namespace Vinapp.Web.Controllers
                 Password = password
             };
 
-            var stringPayload = await Task.Run(() => JsonConvert.SerializeObject(login));
+            var stringPayload = await Task.Run(() => SerializeObject(login));
 
 
             var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
 
-            Auth Auth = null;
-
-            HttpResponseMessage response = await client.PostAsync(path, httpContent);
-            if (response.IsSuccessStatusCode)
-            {
-                //return await response.Content.ReadAsAsync<Auth>().ConfigureAwait(false);
-                if (response.Content != null)
-                    return await JsonConvert.DeserializeObjectAsync<Auth>(await response.Content.ReadAsStringAsync());
-            }
+            var response = await client.PostAsync(path, httpContent);
+            if (!response.IsSuccessStatusCode) return null;
+            if (response.Content != null)
+                return
+                    await await Task.Factory.StartNew(async () => DeserializeObject<Auth>(await response.Content.ReadAsStringAsync()));
             return null;
         }
     }
